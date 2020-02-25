29d78109f74d9fd1bb0a70e9b9293fe57459c21d
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2020-02-25 16:49:35 -0600

    Initial Commit

---
+++ Teaching Demo/Assets/MyStuff/Scripts/CrateSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateSpawner : MonoBehaviour
+{
+    public GameObject cratePrefab;
+
+    [Range(0,100)]
+    public int amountOfCratesToCreate;
+
+    void Start()
+    {
+        //for (int i = 0; i < amountOfCratesToCreate; i++)
+        //{
+        //    Instantiate(cratePrefab, transform.localPosition, Quaternion.identity, this.transform);
+        //}
+
+        int i = 0;
+        while (i < amountOfCratesToCreate)
+        {
+            Instantiate(cratePrefab, transform.localPosition, Quaternion.identity, this.transform);
+            i++;
+        }
+    }
+}
---
+++ Teaching Demo/Assets/MyStuff/Scripts/CrateSpawnerMWB.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateSpawnerMWB : MonoBehaviour
+{
+    public GameObject cratePrefab;
+
+    public int amountOfCratesToSpawn;
+
+    private void Start()
+    {
+        for (int i = 0; i < amountOfCratesToSpawn; i++)
+        {
+            Instantiate(cratePrefab, transform.localPosition, Quaternion.identity, this.transform);
+        }
+    }
+}
